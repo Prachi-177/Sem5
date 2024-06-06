5e4e9a4fb28030b9d4d2dc30fc1bc5e1ae25773d
prachi <prachibaidhya24@gmail.com>
2024-06-06 08:16:47 +0545

    string method

---
+++ stringmethods.js
@@ -0,0 +1,17 @@
+var text = "I am currently involved in BSC. CSIT 5th semseter.";
+//alert(text);
+console.log(text);
+
+// Print the length of the string
+console.log("the length of text is :",text.length);
+
+//Print the character at index 5 of the given string
+console.log("the character at index 5 is:, text.charAt(5)");
+
+//Print the character code at index 5 at the given string
+console.log("the character code at index 5 is:",text.charCodeAt(5));
+
+//Print and write it into the document, the text between 3 and 10 index
+console.log("the text between 3 and 10 index is:",text.slice(3,10));
+document.write("the text between 3 and 10 index is =>",text.slice(3, 40));
+
