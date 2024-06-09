60fc09a68e45867623cd68d450e04a585bc3ea79
Prachi <prachibaidhya24@gmail.com>
2024-06-09 08:23:18 +0545

    Add files via upload

---
+++ arraymethods.js
@@ -0,0 +1,17 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+console.log(`the length of array is ${size}`);
+
+//print the array to string
+console.log(`The string form of given array is ${fruits.toString()}`);
+
+//array at()
+console.log(`The string form of given array is ${fruits.at(2)}`);
+
+//array to join method
+console.log(`The string form of given array is ${fruits.join("*")}`);
+
+//array to pop method
+console.log(`The string form of given array is ${fruits.pop(2)}`);
+
+
